7869b26b2bf23c98e256e338c6ca2954f0ed06fe
David Nicuesa <nicu1309@gmail.com>
2016-04-23 00:30:57 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Space-Apps aircheck #
+
+## Build & Run ##
+
+```sh
+$ cd aircheck
+$ ./sbt
+> jetty:start
+> browse
+```
+
+If `browse` doesn't launch your browser, manually open [http://localhost:8080/](http://localhost:8080/) in your browser.
---
+++ src/main/scala/com/team103/app/MyScalatraServlet.scala
@@ -0,0 +1,16 @@
+package com.team103.app
+
+import org.scalatra._
+
+class MyScalatraServlet extends AircheckStack {
+
+  get("/") {
+    <html>
+      <body>
+        <h1>Hello, world!</h1>
+        Say <a href="hello-scalate">hello to Scalate</a>.
+      </body>
+    </html>
+  }
+
+}

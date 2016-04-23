c0e86a507627db715627b4fa662097fdc667f36f
Ruben Moreno Jimeno <nebur395@hotmail.com>
2016-04-23 18:24:24 +0200

    Add prototype form symptoms

--- src/main/webapp/javascript/controllers/symptomsController.js
+++ src/main/webapp/javascript/controllers/symptomsController.js
@@ -2,4 +2,5 @@ angular.module('AirQApp')
 
     .controller('symptomsCtrl', ['$scope',function($scope){
 
+        $scope.intensity = ["0-None","1-Lowest","2","3","4","5-Median","6","7","8","9","10-Highest"];
     }]);

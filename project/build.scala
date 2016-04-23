94280ecba639fd3eac16f15e2dbfad8672e86647
David Nicuesa <nicu1309@gmail.com>
2016-04-23 14:31:16 +0200

    Update dependencies

--- project/build.scala
+++ project/build.scala
@@ -29,7 +29,12 @@ object AircheckBuild extends Build {
         "org.scalatra" %% "scalatra-specs2" % ScalatraVersion % "test",
         "ch.qos.logback" % "logback-classic" % "1.1.5" % "runtime",
         "org.eclipse.jetty" % "jetty-webapp" % "9.2.15.v20160210" % "container",
-        "javax.servlet" % "javax.servlet-api" % "3.1.0" % "provided"
+        "javax.servlet" % "javax.servlet-api" % "3.1.0" % "provided",
+        "org.clapper" %% "grizzled-slf4j" % "1.0.2",
+        "org.scalatra" %% "scalatra-json" % ScalatraVersion,
+        "org.json4s"   %% "json4s-jackson" % "3.3.0",
+        "org.json4s" %% "json4s-native" % "3.3.0",
+        "postgresql" % "postgresql" % "9.1-901.jdbc4"
       ),
       scalateTemplateConfig in Compile <<= (sourceDirectory in Compile){ base =>
         Seq(

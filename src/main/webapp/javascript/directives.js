90f9e084bd301fe8d912162eca76d97315b0ebbe
Ruben Moreno Jimeno <nebur395@hotmail.com>
2016-04-23 16:25:19 +0200

    Add navbars prototype

--- src/main/webapp/javascript/directives.js
+++ src/main/webapp/javascript/directives.js
@@ -6,10 +6,18 @@
 
 angular.module('AirQApp')
 
-    // include the 'navbar.html' into the <navbar> tag
+    // include the 'navbarTop.html' into the <navbar-top> tag
     .directive('navbarTop', function () {
         return {
             restrict: 'E',
-            templateUrl: 'templates/components/navbar.html'
+            templateUrl: 'templates/components/navbarTop.html'
+        }
+    })
+        
+    // include the 'navbarBottom.html' into the <navbar-bottom> tag
+    .directive('navbarBottom', function () {
+        return {
+            restrict: 'E',
+            templateUrl: 'templates/components/navbarBottom.html'
         }
     });

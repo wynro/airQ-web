2c9c05209fcf416f8f00ebe5baec40176836d650
Ruben Moreno Jimeno <nebur395@hotmail.com>
2016-04-24 04:00:11 +0200

    Add viewData infrastructure page

--- src/main/webapp/javascript/controllers/viewDataController.js
+++ src/main/webapp/javascript/controllers/viewDataController.js
@@ -1,3 +1,36 @@
-/**
- * Created by ruben on 24/04/16.
+/*
+ * AUTHOR: Team 103
+ * FILENAME: symptomsController.js
+ * DESCRIPTION: This file contains the "symptoms.html" controller.
  */
+
+angular.module('AirQApp')
+
+    .controller('viewDataCtrl', ['$scope', function($scope){
+
+
+        $scope.airQualityMap = true;
+        $scope.eventsMap = false;
+        $scope.discordanceMap = false;
+        
+        $scope.viewMap = function (num) {
+            switch (num) {
+                case 1:
+                    $scope.airQualityMap = true;
+                    $scope.eventsMap = false;
+                    $scope.discordanceMap = false;
+                    break;
+                case 2:
+                    $scope.airQualityMap = false;
+                    $scope.eventsMap = true;
+                    $scope.discordanceMap = false;
+                    break;
+                default:
+                    $scope.airQualityMap = false;
+                    $scope.eventsMap = false;
+                    $scope.discordanceMap = true;
+            }
+            
+        };
+
+    }]);
